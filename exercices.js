2f7c70dbb2c9369717a577051aadc8069389cea8
gdudek <gdudek.dev@gmail.com>
2025-02-06 13:23:25 +0100

    Exercice 2: Des informations vous manquent!

--- exercices.js
+++ exercices.js
@@ -4,3 +4,11 @@ const titreBoutique=document.querySelector("h1");
 const nomBoutique=titreBoutique.textContent;
 
 console.log(nomBoutique);
+
+//Des informations manquent !
+
+const paragrapheBienvenue = document.querySelector("#description_boutique");
+const nouveauParagraphe =document.createElement("p");
+
+nouveauParagraphe.textContent="Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter."
+paragrapheBienvenue.appendChild(nouveauParagraphe);
